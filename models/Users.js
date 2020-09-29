58b40a98a1d165cf17a2b2e535feb266e164cbb9
Sean Mattson <mattsos2@gmatc.matc.edu>
2020-09-29 12:27:57 -0500

    first commit of User model

---
+++ models/Users.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = mongoose.model('user', UserSchema); // this is our model for data about 'user'
+
+export default User;
