c6482e5d515c1be63f3c0fb709f04018ea990b3b
Sean Mattson <mattsos2@gmatc.matc.edu>
2020-09-29 15:47:13 -0500

    added data validation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { get } from 'config';
 import express from 'express';
 import connectDatabase from './config/db';
+import {check, validationResult} from 'express-validator';
 
 // Initialize the express application
 const app = express();
@@ -25,9 +26,23 @@ app.get('/', (req, res) =>
  * @route POST api/users
  * @desc Register user
  * */
-app.post('/api/users', (req, res) =>{
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/users',
+    [
+        check ('name', 'Please enter your name.').not().isEmpty(),
+        check ('email', 'Please enter a valid email.').isEmail(),
+        check ('password', 'Please enter a password with 6 or more characters.').isLength({min: 6})                                                                  
+    ],
+(req, res) =>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+    {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    else {
+        return res.send(req.body);
+    }
+    
 });
 
 
