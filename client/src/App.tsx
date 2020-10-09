ddc44f8eeb18bfab51204b1440a69cffe92cc521
Sean Mattson <mattsos2@gmatc.matc.edu>
2020-10-09 12:07:10 -0500

    finished activity-4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        GoodThings
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    Axios.get('http://localhost:5000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          GoodThings
+        </header>
+        {this.state.data}
+      </div>
+    );
+  }
 }
 
 export default App;
