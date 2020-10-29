37304afe4b937a11d212065ab8222a2cf68d25eb
Sean Mattson <mattsos2@gmatc.matc.edu>
2020-10-29 18:35:50 -0500

    finished activity-5

--- server.js
+++ server.js
@@ -80,7 +80,23 @@ async (req, res) =>{
 
                 // Save to db and return
                 await user.save();
-                res.send('User successfully registered');
+
+                // Generate and return a JWT token
+                const payload = {
+                    user: {
+                        id: user.id
+                    }
+                };
+
+                jwt.sign(
+                    payload,
+                    config.get('jwtSecret'),
+                    { expiresIn: '10hr'},
+                    (err, token) => {
+                        if(err) throw err;
+                        res.json({token: token});
+                    }
+                );
             } catch (error) {
                 res.status(500).send('Server error');
             }
